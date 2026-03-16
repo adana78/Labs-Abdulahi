8103db3347a29b630f862d815bb882d5333f77c1
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-03-16 14:35:54 +0300

    part 1

---
+++ Lab Practice/Lab 8 Next.js Web API/Tutorial/B53/myfinance-api/app/api/accounts/route.js
@@ -0,0 +1,42 @@
+// we need to read the json files
+
+import { promises as fs } from "fs"
+import path from "path"
+import { NextResponse } from "next/server";
+
+const filePath = path.join(process.cwd(), "data", "accounts.json")
+
+// read the content of the file located in filepath
+async function readAccounts() {
+    const data = await fs.readFile(filePath);
+    return JSON.parse(data)
+}
+
+async function writeAccounts(accounts) {
+    await fs.writeFile(filePath, JSON.stringify(accounts, null, 4))
+}
+// /api/account
+
+export async function GET(request, { params }) {
+    //return all the accounts
+    const accounts = await readAccounts()
+    return NextResponse.json(accounts)
+}
+export async function POST(request, { params }) {
+    // i will get a new account from the user
+    const account = await request.json()
+
+    // we need to read all the accounts 
+    const accounts = await readAccounts()
+
+    accounts.push(account)
+
+    writeAccounts(accounts)
+
+    // i should write this back to the file
+
+    //add that account to the list of accounts
+    return NextResponse.json({
+        message: "Added the account successful"
+    })
+}
---
+++ Lab Practice/Lab 8 Next.js Web API/Tutorial/B53/myfinance-api/data/end.js
@@ -0,0 +1,11 @@
+// END POINTS WE WANT TO BUILD FOR THE RESOURCE ACCOUNT
+
+// http://localhost:3000/api/accounts -> get all accounts
+// GET /api/accounts -> get all accounts
+// POST /api/accounts -> create a new account
+
+
+// http://localhost:3000/api/accounts/123 -> get account with id 123
+// GET /api/accounts/:id -> get a specific account by id
+// PUT /api/accounts/:id -> update an existing account by id
+// DELETE /api/accounts/:id -> delete an existing account by id
