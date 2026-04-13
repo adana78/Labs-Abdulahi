89dd2b0b91f368d9c89e8d464492b2ca538914b8
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-04-13 11:48:37 +0300

    updated code

--- Lab Practice/Lab 9 Next.js React Pages and Components/Tutorial/B52/my-finance-app/app/transactions/page.jsx
+++ Lab Practice/Lab 9 Next.js React Pages and Components/Tutorial/B52/my-finance-app/app/transactions/page.jsx
@@ -3,24 +3,55 @@ import React, { useEffect, useState } from 'react'
 
 function Transactions() {
 
-    // concepts called react hooks [useState, useEffect]
-    const [counter, setCounter] = useState(0)
+    const [transactions, setTransactions] = useState([])
 
+    // http://localhost:3000/api/transactions?type=income
+    async function getTransactions(type) {
+        const url = type ? `/api/transactions?type=${type}` : '/api/transactions'
+        const res = await fetch(url)
+        const data = await res.json()
+        setTransactions(data)
+    }
+
+    // is a react hook that runs after the component is rendered
     useEffect(() => {
 
-        console.log("I run again", counter);
+        getTransactions()
+    }, [])
 
-    }, [counter])
-
-    function handleIncrement() {
-        setCounter(counter + 1)
-        console.log(counter)
-    }
 
     return (
         <div>
-            <button className={"btn"} onClick={e => handleIncrement()}>Increment</button>
-            <h1>{counter}</h1>
+            <main className="page">
+                <h1>Transactions</h1>
+
+                <div className="filter-bar">
+                    <label htmlFor="type-filter">Filter by type:</label>
+                    <select id="type-filter">
+                        <option value="all">All</option>
+                        <option value="income" >Income</option>
+                        <option value="expense">Expense</option>
+                    </select>
+                </div>
+
+                <div class="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Category</th>
+                                <th>Type</th>
+                                <th>Amount</th>
+                                <th>Date</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            ${transactions.map(t => }
+                        </tbody>
+                    </table>
+                </div>
+            </main>
         </div>
     )
 }
